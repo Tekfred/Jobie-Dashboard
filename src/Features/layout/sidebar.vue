b9f4ea34ad17c8fb11e1db9cca2e9d859ee013ef
Tekfred <asagodfred101@gmail.com>
2026-02-18 15:49:09 +0000

    updated sidebar

--- src/Features/layout/sidebar.vue
+++ src/Features/layout/sidebar.vue
@@ -7,7 +7,6 @@ import logo from "@/assets/images/02_Dashboard.png";
 const route = useRoute();
 const uiStore = useUiStore();
 
-
 // Centralized menu data
 const menuItems = [
   { name: "Dashboard", path: "/", icon: "home" },
@@ -24,51 +23,53 @@ const currentPageName = computed(() => {
 });
 
 const sideMargin = computed(() => {
-  return uiStore.isSidebarOpen ? 'overflow-hidden' : '';
-})
+  return uiStore.isSidebarOpen ? "overflow-hidden" : "";
+});
 
 const sideinfo = computed(() => {
-  return uiStore.isSidebarOpen ? 'opacity-100' : 'opacity-0';
-})
-
+  return uiStore.isSidebarOpen ? "opacity-100" : "opacity-0";
+});
 </script>
 
 <template>
   <aside
-     class="fixed left-0 top-0 h-screen bg-[#4B2AAD] px-6 py-8 text-white shadow-2xl z-20 transition-all duration-300"
+    class="fixed left-0 top-0 h-screen bg-[#4B2AAD] px-6 py-8 text-white shadow-2xl z-20 transition-all duration-300"
     :class="uiStore.isSidebarOpen ? 'w-65' : 'w-20'"
   >
-
-  
-
-
-   <div class="absolute -top-14 left-56 w-20 h-20 bg-[#4B2AAD] -right-3 " :class= 'sideMargin'>
+    <div
+      class="absolute -top-14 left-56 w-20 h-20 bg-[#4B2AAD] -right-3"
+      :class="sideMargin"
+    >
       <div
         class="absolute -bottom-8 -right-5 w-16 h-14 bg-gray-100 rounded-full"
       ></div>
     </div>
     <!-- Logo  hide text when collapsed-->
-    <div class="flex items-center gap-3 mb-12 px-2 overflow-hidden" :class="uiStore.isSidebarOpen ? '' : 'pr-15 relative right-4' ">
-      <div class="w-15 h-15 rounded-xl flex items-center justify-center shrink-0">
+    <div
+      class="flex items-center gap-3 mb-12 px-2 overflow-hidden"
+      :class="uiStore.isSidebarOpen ? '' : 'pr-15 relative right-4'"
+    >
+      <div
+        class="w-15 h-15 rounded-xl flex items-center justify-center shrink-0"
+      >
         <img :src="logo" alt="logo" />
       </div>
-
-      
 
       <!-- 
         👇 The text fades out when sidebar closes
         opacity-100 when open, opacity-0 when closed
       -->
-      <h1  class="text-xl font-bold tracking-wide whitespace-nowrap transition-opacity duration-300"
-        :class="uiStore.isSidebarOpen ? 'opacity-100' : 'opacity-0'">Jobie</h1>
+      <h1
+        class="text-xl font-bold tracking-wide whitespace-nowrap transition-opacity duration-300"
+        :class="uiStore.isSidebarOpen ? 'opacity-100' : 'opacity-0'"
+      >
+        Jobie
+      </h1>
     </div>
 
     <!-- <div
       class="absolute -top-2 left-60 w-20 h-20 bg-[#4B2AAD] -right-2"
     ></div> -->
-
-    
-    
 
     <!-- Navigation -->
     <nav class="space-y-2">
@@ -104,7 +105,9 @@ const sideinfo = computed(() => {
           <!-- Text -->
           <span
             class="font-medium text-sm"
-            :class="isActive ? 'text-[#4B2AAD] font-semibold overflow-hidden' : ''"
+            :class="
+              isActive ? 'text-[#4B2AAD] font-semibold overflow-hidden' : ''
+            "
           >
             {{ item.name }}
           </span>
@@ -115,11 +118,11 @@ const sideinfo = computed(() => {
     <!-- Footer -->
     <div
       class="absolute bottom-10 left-8 text-[10px] text-white/40 tracking-wider"
-     :class="sideinfo" 
+      :class="sideinfo"
     >
-      <p class="font-semibold ">Jobie Job Portal Admin Dashboard</p>
-      <p class="mt-1 ">© 2020 All Rights Reserved</p>
-      <p class="mt-2 ">Made with ❤️ by Peterdraw</p>
+      <p class="font-semibold">Jobie Job Portal Admin Dashboard</p>
+      <p class="mt-1">© 2020 All Rights Reserved</p>
+      <p class="mt-2">Made with ❤️ by Peterdraw</p>
     </div>
   </aside>
 </template>
